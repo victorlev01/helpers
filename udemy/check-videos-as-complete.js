4a47b61580eaa767aec85b03b242bfee2b4fe591
Victor Levitskiy <victorlev01@gmail.com>
2021-07-25 03:30:22 +0700

    Create check-videos-as-complete.js

---
+++ udemy/check-videos-as-complete.js
@@ -0,0 +1,17 @@
+// Udemy - click all videos as checked, for quick exercise - only course;
+// expand all sections
+document.querySelectorAll('.udi-angle-down').forEach(el=>el.click());
+// initialize list of not-watched videos
+var listOfVideoNodesCheckboxes = [];
+var el = document.querySelectorAll('[data-purpose="progress-toggle-button"]:not(:checked)').forEach(el=>{
+ if(el.parentElement.parentElement.querySelector('.udi-play-circle')){
+    // add not watched video clickable element to list
+   listOfVideoNodesCheckboxes.push(el.parentElement);
+ }
+})
+
+// check as completed each one with second interval
+setInterval(function(){
+  var el = listOfVideoNodesCheckboxes.shift();
+  el && el.click();
+},1000);
